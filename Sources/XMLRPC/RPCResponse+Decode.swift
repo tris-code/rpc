124e510daafa20515f42885fd48ba15a25c14984
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-22 04:51:48 +0000

    Move RPCResponse decode to separate file

---
+++ Sources/XMLRPC/RPCResponse+Decode.swift
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import XML
+import Stream
+
+extension RPCResponse {
+    public init<T: UnsafeStreamReader>(from stream: T) throws {
+        let document = try XML.Document(from: stream)
+        guard let root = document.root, root.name == "methodResponse" else {
+            throw RPCError.invalidXML
+        }
+
+        var params = [RPCValue]()
+
+        for param in root["params"].children {
+            guard let value = param["value"].children.first else {
+                throw RPCValueError(
+                    reason: .empty,
+                    description: "xml element has no content")
+            }
+            guard let element = XML.Element(value) else {
+                throw RPCValueError(
+                    reason: .invalidValue,
+                    description: "xml element was expected")
+            }
+            params.append(try RPCValue(from: element))
+        }
+        self.params = params
+    }
+}
