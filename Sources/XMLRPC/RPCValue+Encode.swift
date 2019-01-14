311d8135b768e3d2dc7a67fd24a494dff70968f6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 23:24:12 +0000

    [Cleanup] Delete copyright notice

--- Sources/XMLRPC/RPCValue+Encode.swift
+++ Sources/XMLRPC/RPCValue+Encode.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 import XML
 import Base64
 import struct Foundation.Date
